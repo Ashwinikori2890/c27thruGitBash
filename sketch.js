e3a026a69c0c963d103fc9dc3632a0cc0394c7d4
Ashwinikori2890 <ashwinikori2890@gmail.com>
2021-07-05 16:32:36 +0530

    we added chain fine & modified index and sketch files

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintLog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -12,6 +15,8 @@ function preload() {
 
 function setup(){
     var canvas = createCanvas(1200,400);
+
+    
     engine = Engine.create();
     world = engine.world;
 
@@ -35,6 +40,11 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    constraintLog = new Log(230,180,80,PI/2);
+
+   chain = new Chain(bird.body,constraintLog.body)
+
+   
 
 }
 
@@ -61,4 +71,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constraintLog.display();
+
+    chain.display();
 }
